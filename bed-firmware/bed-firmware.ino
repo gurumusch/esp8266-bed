aef3296f123751a52308920072228195eea555a7
gurumusch <9209378+gurumusch@users.noreply.github.com>
2021-09-12 23:22:41 +0200

    refactoring & number of neopixels configurable

--- bed-firmware/bed-firmware.ino
+++ bed-firmware/bed-firmware.ino
@@ -21,13 +21,20 @@
 #define D7 13
 #define D8 15
 
+#define RIGHT_PIR_0_PIN D2
+#define RIGHT_PIR_1_PIN D3
+
+#define ULTRASOUND_TRIGGER_PIN D0
+#define ULTRASOUND_ECHO_PIN D1
+
+#define NEOPIXEL_PIN D5
+
 #define PAYLOAD_SIZE 100
 char payload[PAYLOAD_SIZE + 1];
 
 /*
    Communication
 */
-//ESP8266WiFiMulti WiFiMulti;
 WiFiClient c;
 PubSubClient mqtt(c);
 #define WIFI_SSID_SIZE 16
@@ -46,6 +53,7 @@ char mqttClient[MQTT_CLIENT_SIZE];
 char mqttTopicDistance[MQTT_TOPIC_SIZE];
 char mqttTopicMotion[MQTT_TOPIC_SIZE];
 char mqttTopicColor[MQTT_TOPIC_SIZE];
+char mqttTopicConfig[MQTT_TOPIC_SIZE];
 int incomingByte = 0;
 
 bool ensureWiFiConnection() {
@@ -87,7 +95,8 @@ bool ensureMQTTConnection() {
     mqtt.setCallback(callback);
 
     if(mqtt.connect(mqttClient, mqttUser, mqttPassword)) {
-      mqtt.subscribe(mqttTopicColor); // 27 + 1
+      mqtt.subscribe(mqttTopicColor);
+      mqtt.subscribe(mqttTopicConfig);
     }
     for (uint8_t i = 0; i < 10; i++) {
       if (mqtt.connected()) {
@@ -105,6 +114,24 @@ bool ensureMQTTConnection() {
   return true;
 }
 
+void callback(char* topic, byte* value, unsigned int length) {
+  sprintf(payload, "MQTT[%s] -> ", topic, length, value[length]);
+  Serial.print(payload);
+  if (length >= PAYLOAD_SIZE) {
+    length = PAYLOAD_SIZE;
+  }
+  memcpy(payload, value, length);
+  payload[length] = '\0';
+  Serial.println(payload);
+
+  if (strncmp(mqttTopicColor, topic, MQTT_TOPIC_SIZE) == 0) {
+    updateColor();
+  }
+  else if (strncmp(mqttTopicConfig, topic, MQTT_TOPIC_SIZE) == 0) {
+    executeCommand();
+  }
+}
+
 void checkIncomingSerial() {
   incomingByte = 0;
   uint8_t i = 0;
@@ -120,132 +147,16 @@ void checkIncomingSerial() {
   if (i > 0) {
     Serial.print("Read from serial: ");
     Serial.println(payload);
-    if (strncmp("writeConfig", payload, PAYLOAD_SIZE) == 0) {
-      Serial.print("writeConfig");
-      writeConfig();
-      Serial.println(" [done]");
-    }
-    else if (strncmp("ssid ", payload, 5) == 0) {
-      strncpy(wifiSsid, payload + 5, WIFI_SSID_SIZE);
-      Serial.print("SSID -> ");
-      Serial.println(wifiSsid);
-    }
-    else if (strncmp("wifiPassword ", payload, 13) == 0) {
-      strncpy(wifiPassword, payload + 13, WIFI_PASSWORD_SIZE);
-      Serial.print("WiFi password -> ");
-      Serial.println(wifiPassword);
-    }
-    else if (strncmp("mqtt broker ", payload, 12) == 0) {
-      strncpy(mqttBroker, payload + 12, MQTT_BROKER_SIZE);
-      Serial.print("MQTT broker -> ");
-      Serial.println(mqttBroker);
-    }
-    else if (strncmp("mqtt user ", payload, 10) == 0) {
-      strncpy(mqttUser, payload + 10, MQTT_USER_SIZE);
-      Serial.print("MQTT user -> ");
-      Serial.println(mqttUser);
-    }
-    else if (strncmp("mqtt password ", payload, 14) == 0) {
-      strncpy(mqttPassword, payload + 14, MQTT_PASSWORD_SIZE);
-      Serial.print("MQTT password -> ");
-      Serial.println(mqttPassword);
-    }
-    else if (strncmp("mqtt client ", payload, 12) == 0) {
-      strncpy(mqttClient, payload + 12, MQTT_CLIENT_SIZE);
-      Serial.print("MQTT client -> ");
-      Serial.println(mqttClient);
-    }
-    else if (strncmp("mqtt topic distance ", payload, 20) == 0) {
-      strncpy(mqttTopicDistance, payload + 20, MQTT_TOPIC_SIZE);
-      Serial.print("MQTT topic distance -> ");
-      Serial.println(mqttTopicDistance);
-    }
-    else if (strncmp("mqtt topic color ", payload, 17) == 0) {
-      strncpy(mqttTopicColor, payload + 17, MQTT_TOPIC_SIZE);
-      Serial.print("MQTT topic color -> ");
-      Serial.println(mqttTopicColor);
-    }
-    else if (strncmp("mqtt topic motion ", payload, 18) == 0) {
-      strncpy(mqttTopicMotion, payload + 18, MQTT_TOPIC_SIZE);
-      Serial.print("MQTT topic motion -> ");
-      Serial.println(mqttTopicMotion);
-    }
-    else if (strncmp("disconnect wifi", payload, 15) == 0) {
-      WiFi.disconnect();
-      Serial.println("disconnecting WiFi");
-    }
-    else if (strncmp("disconnect mqtt", payload, 15) == 0) {
-      WiFi.disconnect();
-      Serial.println("disconnecting MQTT");
-    }
+    executeCommand();
   }
-}
-
-void writeConfig() {
-  uint16_t start = 0;
-  writeEEPROM(wifiSsid, start, WIFI_SSID_SIZE);
-  start += WIFI_SSID_SIZE;
-  writeEEPROM(wifiPassword, start, WIFI_PASSWORD_SIZE);
-  start += WIFI_PASSWORD_SIZE;
-  writeEEPROM(mqttBroker, start, MQTT_BROKER_SIZE);
-  start += MQTT_BROKER_SIZE;
-  writeEEPROM(mqttUser, start, MQTT_USER_SIZE);
-  start += MQTT_USER_SIZE;
-  writeEEPROM(mqttPassword, start, MQTT_PASSWORD_SIZE);
-  start += MQTT_PASSWORD_SIZE;
-  writeEEPROM(mqttClient, start, MQTT_CLIENT_SIZE);
-  start += MQTT_CLIENT_SIZE;
-  writeEEPROM(mqttTopicDistance, start, MQTT_TOPIC_SIZE);
-  start += MQTT_TOPIC_SIZE;
-  writeEEPROM(mqttTopicColor, start, MQTT_TOPIC_SIZE);
-  start += MQTT_TOPIC_SIZE;
-  writeEEPROM(mqttTopicMotion, start, MQTT_TOPIC_SIZE);
-  start += MQTT_TOPIC_SIZE;
-}
-
-void loadConfig() {
-  uint16_t start = 0;
-  readEEPROM(wifiSsid, start, WIFI_SSID_SIZE);
-  start += WIFI_SSID_SIZE;
-  readEEPROM(wifiPassword, start, WIFI_PASSWORD_SIZE);
-  start += WIFI_PASSWORD_SIZE;
-  readEEPROM(mqttBroker, start, MQTT_BROKER_SIZE);
-  start += MQTT_BROKER_SIZE;
-  readEEPROM(mqttUser, start, MQTT_USER_SIZE);
-  start += MQTT_USER_SIZE;
-  readEEPROM(mqttPassword, start, MQTT_PASSWORD_SIZE);
-  start += MQTT_PASSWORD_SIZE;
-  readEEPROM(mqttClient, start, MQTT_CLIENT_SIZE);
-  start += MQTT_CLIENT_SIZE;
-  readEEPROM(mqttTopicDistance, start, MQTT_TOPIC_SIZE);
-  start += MQTT_TOPIC_SIZE;
-  readEEPROM(mqttTopicColor, start, MQTT_TOPIC_SIZE);
-  start += MQTT_TOPIC_SIZE;
-  readEEPROM(mqttTopicMotion, start, MQTT_TOPIC_SIZE);
-  start += MQTT_TOPIC_SIZE;
-}
-
-void writeEEPROM(char* value, int startAt, uint8_t len) {
-  EEPROM.begin(512);
-  for (int i = 0; i < len; i++) {
-    EEPROM.write(startAt + i, value[i]);
-  }
-  EEPROM.end();
-}
-
-void readEEPROM(char* dest, int startAt, uint8_t len) {
-  EEPROM.begin(512);
-  for (int i = 0; i < len; i++) {
-    dest[i] = EEPROM.read(startAt + i);
-  }
-  EEPROM.end();
 }
 
 /*
    Lights
 */
 #define NUMPIXELS 1
-Adafruit_NeoPixel pixels(NUMPIXELS, D8, NEO_GRB + NEO_KHZ800);
+uint16_t num_pixels = NUMPIXELS;
+Adafruit_NeoPixel pixels(num_pixels, NEOPIXEL_PIN, NEO_RGB + NEO_KHZ800);
 
 void setAllPixelsToColor(uint8_t r, uint8_t g, uint8_t b) {
   uint32_t color = Adafruit_NeoPixel::Color(r, g, b);
@@ -256,21 +167,18 @@ void setAllPixelsToColor(uint8_t r, uint8_t g, uint8_t b) {
   pixels.show();
 }
 
-void callback(char* topic, byte* value, unsigned int length) {
-  sprintf(payload, "MQTT[%s] -> ", topic, length, value[length]);
-  Serial.print(payload);
-  if (length >= PAYLOAD_SIZE) {
-    length = PAYLOAD_SIZE;
-  }
-  memcpy(payload, value, length);
-  payload[length] = '\0';
-  Serial.println(payload);
-
-  uint32_t red = 2;
-  uint32_t green = 2;
-  uint32_t blue = 2;
-  sscanf(payload, "%u,%u,%u", &red, &green, &blue);
-  setAllPixelsToColor(red, green, blue);
+void updateColor() {
+    uint32_t red = 2;
+    uint32_t green = 2;
+    uint32_t blue = 2;
+    sscanf(payload, "%u,%u,%u", &red, &green, &blue);
+    Serial.print("New color: ");
+    Serial.print(red, DEC);
+    Serial.print(", ");
+    Serial.print(green, DEC);
+    Serial.print( ", ");
+    Serial.println(blue, DEC);
+    setAllPixelsToColor(red, green, blue);
 }
 
 /*
@@ -282,15 +190,15 @@ float distanceMm;
 
 void measureDistance() {
   // Clears the trigPin
-  digitalWrite(D7, LOW);
+  digitalWrite(ULTRASOUND_TRIGGER_PIN, LOW);
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(D7, HIGH);
+  digitalWrite(ULTRASOUND_TRIGGER_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(D7, LOW);
+  digitalWrite(ULTRASOUND_TRIGGER_PIN, LOW);
 
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(D6, HIGH);
+  duration = pulseIn(ULTRASOUND_ECHO_PIN, HIGH);
 
   // Calculate the distance
   distanceMm = duration * SOUND_VELOCITY / 2;
@@ -304,18 +212,193 @@ void measureDistance() {
 }
 
 /*
+   Config
+*/
+void executeCommand() {
+  if (strncmp("writeConfig", payload, PAYLOAD_SIZE) == 0) {
+    Serial.print("writeConfig");
+    writeConfig();
+    Serial.println(" [done]");
+  }
+  else if (strncmp("ssid ", payload, 5) == 0) {
+    strncpy(wifiSsid, payload + 5, WIFI_SSID_SIZE);
+    Serial.print("SSID -> ");
+    Serial.println(wifiSsid);
+  }
+  else if (strncmp("wifiPassword ", payload, 13) == 0) {
+    strncpy(wifiPassword, payload + 13, WIFI_PASSWORD_SIZE);
+    Serial.print("WiFi password -> ");
+    Serial.println(wifiPassword);
+  }
+  else if (strncmp("mqtt broker ", payload, 12) == 0) {
+    strncpy(mqttBroker, payload + 12, MQTT_BROKER_SIZE);
+    Serial.print("MQTT broker -> ");
+    Serial.println(mqttBroker);
+  }
+  else if (strncmp("mqtt user ", payload, 10) == 0) {
+    strncpy(mqttUser, payload + 10, MQTT_USER_SIZE);
+    Serial.print("MQTT user -> ");
+    Serial.println(mqttUser);
+  }
+  else if (strncmp("mqtt password ", payload, 14) == 0) {
+    strncpy(mqttPassword, payload + 14, MQTT_PASSWORD_SIZE);
+    Serial.print("MQTT password -> ");
+    Serial.println(mqttPassword);
+  }
+  else if (strncmp("mqtt client ", payload, 12) == 0) {
+    strncpy(mqttClient, payload + 12, MQTT_CLIENT_SIZE);
+    Serial.print("MQTT client -> ");
+    Serial.println(mqttClient);
+  }
+  else if (strncmp("mqtt topic distance ", payload, 20) == 0) {
+    strncpy(mqttTopicDistance, payload + 20, MQTT_TOPIC_SIZE);
+    Serial.print("MQTT topic distance -> ");
+    Serial.println(mqttTopicDistance);
+  }
+  else if (strncmp("mqtt topic color ", payload, 17) == 0) {
+    strncpy(mqttTopicColor, payload + 17, MQTT_TOPIC_SIZE);
+    Serial.print("MQTT topic color -> ");
+    Serial.println(mqttTopicColor);
+  }
+  else if (strncmp("mqtt topic motion ", payload, 18) == 0) {
+    strncpy(mqttTopicMotion, payload + 18, MQTT_TOPIC_SIZE);
+    Serial.print("MQTT topic motion -> ");
+    Serial.println(mqttTopicMotion);
+  }
+  else if (strncmp("mqtt topic config ", payload, 18) == 0) {
+    strncpy(mqttTopicConfig, payload + 18, MQTT_TOPIC_SIZE);
+    Serial.print("MQTT topic config -> ");
+    Serial.println(mqttTopicConfig);
+  }
+  else if (strncmp("disconnect wifi", payload, 15) == 0) {
+    WiFi.disconnect();
+    Serial.println("disconnecting WiFi");
+  }
+  else if (strncmp("disconnect mqtt", payload, 15) == 0) {
+    WiFi.disconnect();
+    Serial.println("disconnecting MQTT");
+  }
+  else if (strncmp("dump config", payload, 11) == 0) {
+    Serial.println("Config:");
+    Serial.print("WIFI SSID: ");
+    Serial.println(wifiSsid);
+    Serial.print("WIFI Password: ");
+    Serial.println(wifiPassword);
+    Serial.print("MQTT Broker: ");
+    Serial.println(mqttBroker);
+    Serial.print("MQTT User: ");
+    Serial.println(mqttUser);
+    Serial.print("MQTT Password: ");
+    Serial.println(mqttPassword);
+    Serial.print("MQTT Client: ");
+    Serial.println(mqttClient);
+    Serial.print("MQTT Topic Distance: ");
+    Serial.println(mqttTopicDistance);
+    Serial.print("MQTT Topic Color: ");
+    Serial.println(mqttTopicColor);
+    Serial.print("MQTT Topic Motion: ");
+    Serial.println(mqttTopicMotion);
+    Serial.print("MQTT Topic Config: ");
+    Serial.println(mqttTopicConfig);
+    Serial.print("# Pixels: ");
+    Serial.println(num_pixels, DEC);
+  }
+}
+
+void writeConfig() {
+  uint16_t start = 0;
+  writeString2EEPROM(wifiSsid, start, WIFI_SSID_SIZE);
+  start += WIFI_SSID_SIZE;
+  writeString2EEPROM(wifiPassword, start, WIFI_PASSWORD_SIZE);
+  start += WIFI_PASSWORD_SIZE;
+  writeString2EEPROM(mqttBroker, start, MQTT_BROKER_SIZE);
+  start += MQTT_BROKER_SIZE;
+  writeString2EEPROM(mqttUser, start, MQTT_USER_SIZE);
+  start += MQTT_USER_SIZE;
+  writeString2EEPROM(mqttPassword, start, MQTT_PASSWORD_SIZE);
+  start += MQTT_PASSWORD_SIZE;
+  writeString2EEPROM(mqttClient, start, MQTT_CLIENT_SIZE);
+  start += MQTT_CLIENT_SIZE;
+  writeInteger2EEPROM(5, start);
+  start += 2;
+  writeString2EEPROM(mqttTopicDistance, start, MQTT_TOPIC_SIZE);
+  start += MQTT_TOPIC_SIZE;
+  writeString2EEPROM(mqttTopicColor, start, MQTT_TOPIC_SIZE);
+  start += MQTT_TOPIC_SIZE;
+  writeString2EEPROM(mqttTopicMotion, start, MQTT_TOPIC_SIZE);
+  start += MQTT_TOPIC_SIZE;
+  writeString2EEPROM(mqttTopicConfig, start, MQTT_TOPIC_SIZE);
+  start += MQTT_TOPIC_SIZE;
+}
+
+void loadConfig() {
+  uint16_t start = 0;
+  readStringFromEEPROM(wifiSsid, start, WIFI_SSID_SIZE);
+  start += WIFI_SSID_SIZE;
+  readStringFromEEPROM(wifiPassword, start, WIFI_PASSWORD_SIZE);
+  start += WIFI_PASSWORD_SIZE;
+  readStringFromEEPROM(mqttBroker, start, MQTT_BROKER_SIZE);
+  start += MQTT_BROKER_SIZE;
+  readStringFromEEPROM(mqttUser, start, MQTT_USER_SIZE);
+  start += MQTT_USER_SIZE;
+  readStringFromEEPROM(mqttPassword, start, MQTT_PASSWORD_SIZE);
+  start += MQTT_PASSWORD_SIZE;
+  readStringFromEEPROM(mqttClient, start, MQTT_CLIENT_SIZE);
+  start += MQTT_CLIENT_SIZE;
+  num_pixels = readIntegerFromEEPROM(start);
+  start += 2;
+  readStringFromEEPROM(mqttTopicDistance, start, MQTT_TOPIC_SIZE);
+  start += MQTT_TOPIC_SIZE;
+  readStringFromEEPROM(mqttTopicColor, start, MQTT_TOPIC_SIZE);
+  start += MQTT_TOPIC_SIZE;
+  readStringFromEEPROM(mqttTopicMotion, start, MQTT_TOPIC_SIZE);
+  start += MQTT_TOPIC_SIZE;
+  readStringFromEEPROM(mqttTopicConfig, start, MQTT_TOPIC_SIZE);
+  start += MQTT_TOPIC_SIZE;
+}
+
+void writeString2EEPROM(char* value, uint16_t startAt, uint8_t len) {
+  EEPROM.begin(512);
+  for (int i = 0; i < len; i++) {
+    EEPROM.write(startAt + i, value[i]);
+  }
+  EEPROM.end();
+}
+
+void writeInteger2EEPROM(uint16_t value, uint16_t startAt) {
+  const unsigned char high = ((value >> 7) & 0x7f) | 0x80;
+  const unsigned char low  = (value & 0x7f);
+
+  EEPROM.begin(512);
+  EEPROM.write(startAt, high);
+  EEPROM.write(startAt + 1, low);
+  EEPROM.end();  
+}
+
+void readStringFromEEPROM(char* dest, uint16_t startAt, uint8_t len) {
+  EEPROM.begin(512);
+  for (int i = 0; i < len; i++) {
+    dest[i] = EEPROM.read(startAt + i);
+  }
+  EEPROM.end();
+}
+
+uint16_t readIntegerFromEEPROM(uint16_t startAt) {
+  EEPROM.begin(512);
+  const unsigned char high = EEPROM.read(startAt);
+  const unsigned char low  = EEPROM.read(startAt + 1);
+  EEPROM.end();
+  return (((high & 0x7f) <<  7) | low);
+}
+
+/*
    Arduino functions
 */
 void setup() {
-  pinMode(D0, INPUT_PULLUP);
-  pinMode(D1, INPUT);
-  pinMode(D2, INPUT);
-  pinMode(D3, INPUT);
-  pinMode(D4, INPUT);
-  pinMode(D5, INPUT);
-  pinMode(D6, INPUT);
-  pinMode(D7, OUTPUT);
-  //  pinMode(D8, OUTPUT);
+  pinMode(RIGHT_PIR_0_PIN, INPUT);
+  pinMode(RIGHT_PIR_1_PIN, INPUT);
+  pinMode(ULTRASOUND_TRIGGER_PIN, OUTPUT);
+  pinMode(ULTRASOUND_ECHO_PIN, INPUT);
 
   Serial.begin(9600);
 
@@ -323,6 +406,7 @@ void setup() {
 
   loadConfig();
 
+  pixels.updateLength(num_pixels);
   pixels.begin();
 
   setAllPixelsToColor(0, 0, 0);
